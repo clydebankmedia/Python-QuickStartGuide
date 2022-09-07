eb1ed6cd68dab4099776bf0c0ffb844d217a598c
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-07 15:54:52 -0400

    Create Snippet_03-07.py

---
+++ 03-Chapter 3: Controlling Program Flow/Snippet_03-07.py
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+c = 3
+
+if a > b:
+    print("a is greater than b")
+    if b != c:
+        print("but b is not equal to c")
+    else:
+        print("b is less than c")
+else:
+    print("a is less than b")
