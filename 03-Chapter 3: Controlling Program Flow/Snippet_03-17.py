1acf17cbf098efac7934a4bc693998482997355b
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-07 16:24:43 -0400

    Create Snippet_03-17.py

---
+++ 03-Chapter 3: Controlling Program Flow/Snippet_03-17.py
@@ -0,0 +1,7 @@
+bottles = 100
+while bottles > 0:
+	print(str(bottles) + " bottles of beer on the wall. ")
+	print(str(bottles) + " bottles of beer.")
+	bottles -= 1
+	print("Take one down, pass it around, ")
+	print(str(bottles) + " bottles of beer on the wall.")
