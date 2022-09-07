308b615dcbad34c42275078f091d4cbced9914ce
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-07 16:18:27 -0400

    Create Snippet_03-15.py

---
+++ 03-Chapter 3: Controlling Program Flow/Snippet_03-15.py
@@ -0,0 +1,6 @@
+# The planets
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+# Display the planet and their number
+for index, value in enumerate(planets):
+	print("Planet " + str(index + 1) + ": " + value)
