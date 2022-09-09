3692301ffa39b31354dcc4634a1ee9250a5d48b3
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 13:37:35 -0400

    Create Snippet_10-07.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_10-07.py
@@ -0,0 +1,8 @@
+# New glossary terms
+glossary = "delimiter, module, package, class, object"
+
+# Split by comma then a space
+glossary_list = glossary.split(", ")
+
+# Display it
+print(glossary_list)
