e284d7bbfb67e567f014463e421b616188abea9d
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 09:43:40 -0400

    Create Snippet_04-02.py

---
+++ 04-Chapter 4: Handling Errors/Snippet_04-02.py
@@ -0,0 +1,10 @@
+# Divide a number by zero
+a = 7
+b = 0
+
+try:
+    print(str(a) + " divided by " + str(b) + " is " + str(a / b))
+except:
+    print("Sorry, a problem occurred dividing the numbers.")
+
+print("All done!")
