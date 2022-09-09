0158a9cdcd46458a0b9e4c0ddc8392cabe785fdd
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 14:28:10 -0400

    Create Snippet_10-21.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_10-21.py
@@ -0,0 +1,5 @@
+# Define our greeting
+greeting = "Hello, {name}! It's currently {temp} and the time is {time}."
+
+# Print the message
+print(greeting.format(name = "Robert", temp = "54F", time = "3:42 PM"))
