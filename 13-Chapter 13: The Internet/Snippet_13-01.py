7ce200054badefdb4d5e1d81d93642e44724c64d
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 15:16:19 -0400

    Create Snippet_13-01.py

---
+++ 13-Chapter 13: The Internet/Snippet_13-01.py
@@ -0,0 +1,10 @@
+# Import the urllib module
+import urllib.request
+
+# URL to GET
+url = "https://www.python.org/"
+
+# Get the page content
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+page_content = response.read()
