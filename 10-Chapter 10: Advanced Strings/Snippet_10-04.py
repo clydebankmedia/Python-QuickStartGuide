49f6f32931c7331ec7973fd246ef22ef5ad94e0f
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 13:31:23 -0400

    Create Snippet_10-04.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_10-04.py
@@ -0,0 +1,6 @@
+# Create our string
+tongue_twister = "She sells seashells by the seashore."
+
+# Count the number of 's' in tongue_twister and display it
+print("There are " + str(tongue_twister.count("s")) + " letter s in:")
+print(tongue_twister)
