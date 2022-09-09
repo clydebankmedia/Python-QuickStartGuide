95cfd79239e01fd0e20100c056afa6bf3ef1d959
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 13:28:51 -0400

    Create Snippet_10-02.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_10-02.py
@@ -0,0 +1,10 @@
+# Create a string
+a = "Hello, World!"
+
+# Search for "World" in the string
+if a.find("World") != -1:
+
+	# Replace "World" with "Reader"
+	print(a)
+	print("... was replaced with ...")
+	print(a.replace("World", "Reader"))
