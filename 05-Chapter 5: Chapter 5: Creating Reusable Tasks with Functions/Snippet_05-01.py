afc67c0e5569c20493263a850ca44f6350ef3d68
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 09:51:52 -0400

    Create Snippet_05-01.py

---
+++ 05-Chapter 5: Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-01.py
@@ -0,0 +1,7 @@
+# Define the ask function
+def ask(prompt):
+    return input(prompt + " ")
+
+# Use the ask function to find out how many cups we want
+question = ask("How many cups do you want?")
+print(question)
