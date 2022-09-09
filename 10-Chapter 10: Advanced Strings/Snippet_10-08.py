fc362a762a462e10cac5d069649ad88e75a7c58a
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 13:38:57 -0400

    Create Snippet_10-08.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_10-08.py
@@ -0,0 +1,8 @@
+# Our glossary terms
+glossary = ['delimiter', 'module', 'package', 'class', 'object']
+
+# The new joined string
+glossary_string = ", ".join(glossary)
+
+# Display it
+print(glossary_string)
