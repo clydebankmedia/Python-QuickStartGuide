a14459d0560e71fb487b29d1575aed01f42d314b
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 14:54:59 -0400

    Create Snippet_11-05.py

---
+++ 11-Chapter 11: Math in Python/Snippet_11-05.py
@@ -0,0 +1,13 @@
+# Import the Decimal class from the decimal module
+from decimal import Decimal
+
+# Define some numbers
+a = Decimal("0.2")
+b = Decimal("0.2")
+c = Decimal("0.2")
+
+# Add them together
+result = a + b + c
+
+# Display the result as a string
+print(str(result))
