2e86fdf5e8fc93cc5dfa569377aa7a23fea04bc1
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-09 11:22:10 -0400

    Create Snippet_06-01.py

---
+++ 06-Chapter 6: Classes/Snippet_06-01.py
@@ -0,0 +1,12 @@
+# Define the World class
+class World:
+    # Define our greeting
+    greeting = "Hello, World!"
+
+    # Run this whenever the object is created
+    def __init__(self):
+        # Print the greeting
+        print(self.greeting)
+
+# Use the class World to create a world object named w
+w = World()
