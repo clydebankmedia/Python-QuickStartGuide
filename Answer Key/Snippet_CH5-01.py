9a32e1750659e1b4784fa827989f26e4ff404071
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-12 14:34:43 -0400

    Create Snippet_CH5-01.py

---
+++ Answer Key/Snippet_CH5-01.py
@@ -0,0 +1,22 @@
+# Define the bottles_song function with the start argument defaulting to 1
+def bottles_song(start=1):
+    # Set the initial number of bottles to the start argument
+    bottles = start
+    # Loop through until bottles are restored
+    while bottles <= 100:
+        # Display the song
+        this_verse = []
+        this_verse.append(str(bottles) + " bottles of beer on the wall. ")
+        this_verse.append(str(bottles) + " bottles of beer. ")
+        this_verse.append("Take one down, pass it around, ")
+        this_verse.append(str(bottles) + " bottles of beer on the wall. ")
+        # Add a bottle
+        bottles += 1
+        # Yield to the calling function
+        yield "".join(this_verse)
+        # Pick back up here when we return
+    return True
+
+# Loop through the generator
+for v in bottles_song():
+    print(v)
