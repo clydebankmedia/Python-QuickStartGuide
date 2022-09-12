03a876f8bbf3bb24f18eed723de4d619e97e3177
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-12 13:24:33 -0400

    Delete 05-Chapter 5: Chapter 5: Creating Reusable Tasks with Functions directory

--- 05-Chapter 5: Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-02.py
+++
@@ -1,6 +0,0 @@
-# Define the ask function
-def ask(prompt):
-    return input(prompt + " ")
-
-# Use the ask function to find out how many cups we want
-print(ask("How many cups do you want?"))
--- 05-Chapter 5: Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-04.py
+++
@@ -1,8 +0,0 @@
-def ask(prompt="Please enter a value: "):
-    return input(prompt + " ")
-
-a = ask()
-print(a)
-
-a = ask()
-b = ask("What do you want for b?")
