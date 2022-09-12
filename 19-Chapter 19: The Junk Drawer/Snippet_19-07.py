f4594311d9bee8e12bd2bd67e6caec3e55bb72a6
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-12 14:05:16 -0400

    Create Snippet_19-07.py

---
+++ 19-Chapter 19: The Junk Drawer/Snippet_19-07.py
@@ -0,0 +1,14 @@
+# Import the hashlib module
+import hashlib
+
+# Define two sets of data
+original_data = "The quick brown foxed jumped over the lazy red dog."
+new_data = "The quick brown foxed leaped over the spry orange cat."
+
+# Hash them both
+original_hash = hashlib.sha256(original_data.encode()).hexdigest()
+new_hash = hashlib.sha256(new_data.encode()).hexdigest()
+
+# Display the results
+print("Original SHA256 hash: " + original_hash)
+print("New SHA256 hash: " + new_hash)
