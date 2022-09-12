6a165b71822a7bab38274f844f3d8fb869639357
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-12 14:37:01 -0400

    Create Snippet_CH7-01.py

---
+++ Answer Key/Snippet_CH7-01.py
@@ -0,0 +1,79 @@
+# A class of player (i.e., fighter, mage, rogue, etc.)
+class PlayerClass:
+    pass
+
+# The brave fighter
+class Fighter(PlayerClass):
+    pass
+
+# The wise mage
+class Mage(PlayerClass):
+    pass
+
+# The sneaky rogue
+class Rogue(PlayerClass):
+    pass
+
+# The powerful healer
+class Healer(PlayerClass):
+    pass
+
+# Our player
+class Player:
+    pass
+
+# Weapon base class 
+class Weapon:
+    pass
+
+# A short sword
+class ShortSword(Weapon):
+    pass
+
+# A long sword
+class LongSword(Weapon):
+    pass
+
+# A bow
+class Bow(Weapon):
+    pass
+
+# A dagger
+class Dagger(Weapon):
+    pass
+
+# Armor base class
+class Armor:
+    pass
+
+# Leather armor
+class LeatherArmor(Armor):
+    pass
+
+# Plate armor
+class PlateArmor(Armor):
+    pass
+
+# A shield
+class Shield(Armor):
+    pass
+
+# Base spell class
+class Spell:
+    pass
+
+# A fireball spell
+class Fireball(Spell):
+    pass
+
+# A simple healing spell
+class Heal(Spell):
+    pass
+
+# The monster
+class Monster:
+    pass
+
+# A basilisk (don't look!)
+class Basilisk(Monster):
+    pass
