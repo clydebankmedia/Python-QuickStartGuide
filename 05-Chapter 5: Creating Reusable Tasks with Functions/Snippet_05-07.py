ada20fb0bb0c445a78e98f097447dd2fe3c49918
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2022-09-12 16:06:11 -0400

    Create Snippet_05-07.py

---
+++ 05-Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-07.py
@@ -0,0 +1,24 @@
+
+# Define the generator function
+# with the start argument defaulting to 100
+def bottles_song(start=100):
+    # Set the initial number of bottles to the start argument
+    bottles = start
+    # Loop through until bottles are gone
+    while bottles > 0:
+        # Display the song
+        print(str(bottles) + " bottles of beer on the wall. ")
+        print(str(bottles) + " bottles of beer.")
+        print("Take one down, pass it around, ")
+        print(str(bottles) + " bottles of beer on the wall.")
+        # Subtract a bottle
+        bottles -= 1
+        # Yield to the calling function
+        yield
+    # Pick back up here when we return
+    return True
+
+# Loop through the generator
+for i in bottles_song():
+    # Don't do anything as the generator does the printing
+    pass
