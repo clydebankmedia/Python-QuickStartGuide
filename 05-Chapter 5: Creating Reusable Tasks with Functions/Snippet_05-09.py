404ee362ce4583c6c9f192a1451087b65e26a41d
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2023-03-16 14:52:11 -0400

    Update Snippet_05-09.py

--- 05-Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-09.py
+++ 05-Chapter 5: Creating Reusable Tasks with Functions/Snippet_05-09.py
@@ -1,5 +1,5 @@
-# Define the bottles_song function with the start argument defaulting to 100
-def bottles_song(start=100):
+# Define the bottles_song function with the start argument defaulting to 99
+def bottles_song(start=99):
     # Set the initial number of bottles to the start argument
     bottles = start
     # Loop through until bottles are gone
