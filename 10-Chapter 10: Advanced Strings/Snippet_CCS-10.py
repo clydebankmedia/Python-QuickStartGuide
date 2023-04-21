d34112ada995485daddeae020d41989963de93c4
Scott K <75698142+ClydeBankScott@users.noreply.github.com>
2023-04-21 12:22:28 -0400

    Create Snippet_CCS-10.py

---
+++ 10-Chapter 10: Advanced Strings/Snippet_CCS-10.py
@@ -0,0 +1,24 @@
+# ClydeBank Coffee Shop Simulator 4000
+# Copyright 2022 (C) ClydeBank Media, All Rights Reserved.
+
+# Import all functions from the utility module
+from utilities import *
+
+# Import the game class from the coffee_shop_simulator module
+from coffee_shop_simulator import CoffeeShopSimulator
+
+# Print welcome message
+welcome()
+
+# Get name and store name
+t_name = prompt("What is your name?", True)
+t_shop_name = prompt("What do you want to name your coffee shop?", True)
+
+# Create the game object
+game = CoffeeShopSimulator(t_name, t_shop_name)
+
+# Run the game
+game.run()
+
+# Say goodbye!
+print("\nThanks for playing. Have a great rest of your day!\n")
